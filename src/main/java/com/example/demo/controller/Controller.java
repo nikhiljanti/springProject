4797617184f7abcae016a29814944c61152a12df
nikhiljanti <nikhil.janti@gmail.com>
2022-06-30 16:33:41 +0530

    First commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/path/{name}")
+	@ResponseBody
+	public String showData(@PathVariable("name") String name){
+		return "Hi "+name;
+	}
+	
+
+}
